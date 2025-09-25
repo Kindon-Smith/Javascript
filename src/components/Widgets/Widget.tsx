cee5035ce157e6b24e32b1e47caa99e2f9b648af
Kindon-Smith <kindon@bluelim3.com>
2025-09-25 15:33:52 -0700

    Updated filestructure and adding base widget.tsx file for inheritance based widgets.

---
+++ src/components/Widgets/Widget.tsx
@@ -0,0 +1,8 @@
+<div className="widget">
+    <div className="widget-header">
+      {/* title and refresh button */}
+    </div>
+    <div className="widget-content">
+      {/* children or loading/error states */}
+    </div>
+</div>
